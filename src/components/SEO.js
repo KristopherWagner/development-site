b17ffe1135256f1ba79b8842938a03998eb86c00
KristopherWagner <kmwagner91@gmail.com>
2023-10-17 11:48:01 -0400

    :recycle: Add a reusable component for SEO

---
+++ src/components/SEO.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Seo({ description, imageUrl, title, url }) {
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content="nofollow, noimageindex, noarchive" />
+      <link rel="canonical" href={url} />
+
+      {/* Facebook */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="kwagner.dev" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+    </>
+  );
+}
