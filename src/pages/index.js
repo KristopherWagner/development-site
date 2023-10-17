8dce4430a6f0d43eda67781ad11ed9b3e53c1586
KristopherWagner <kmwagner91@gmail.com>
2023-10-17 11:40:48 -0400

    :sparkles: Add SEO to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,3 +12,35 @@ export default function IndexPage() {
     </>
   );
 }
+
+export function Head() {
+  const title = "Kristopher Wagner's Development Site";
+  const url = 'https://kwagner.dev';
+  const description = 'A page dedicated to my personal coding projects';
+  const imageUrl =
+    'https://kwagner.dev/static/starlight_lounge-cf67d74b7be4b4baf494389267fa8e60.webp';
+
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content="nofollow, noimageindex, noarchive" />
+      <link rel="canonical" href={url} />
+
+      {/* Facebook */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="kwagner.dev" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+    </>
+  );
+}
