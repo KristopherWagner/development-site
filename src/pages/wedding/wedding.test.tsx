c108e7a8b86e0cc985c1c6ef7a8c13faf5ecda81
KristopherWagner <kmwagner91@gmail.com>
2024-12-11 11:51:50 -0500

    :white_check_mark: Add unit tests

---
+++ src/pages/wedding/wedding.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import WeddingPage from '.';
+
+jest.mock('@fontsource/montserrat', () => jest.fn());
+jest.mock('@fontsource/roboto', () => jest.fn());
+
+describe('wedding page tests', () => {
+  it.each([
+    'Our Wedding Day',
+    'Travel and Accommodations',
+    'COVID-19',
+    'The Wedding Party',
+    'Schedule of Events',
+    'Frequently Asked Questions',
+  ])('should render the heading %s', (heading) => {
+    render(<WeddingPage />);
+    expect(
+      screen.queryByRole('heading', { name: heading })
+    ).toBeInTheDocument();
+  });
+});
