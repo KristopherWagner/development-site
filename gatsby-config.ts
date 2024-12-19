1c59a795984cb758a5d7b6829f80a3d9a126a8ec
KristopherWagner <kmwagner91@gmail.com>
2024-12-19 16:01:15 -0500

    Revert ":heavy_minus_sign: Remove gatsby-source-filesystem"
    This reverts commit 7cd1b502e86b171361b5530b2351a757c13da5cb.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,5 +5,13 @@ module.exports = {
     author: '@KristopherWagner',
     siteUrl: `localhost:8000`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 };
