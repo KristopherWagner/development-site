95f7ea1ea7376b7dab7ca91cc097096d74cc5e0d
KristopherWagner <kmwagner91@gmail.com>
2025-04-25 16:48:08 -0400

    :bug: Fix styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,13 @@ import '@fontsource/montserrat';
 import '@fontsource/roboto';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 
+import './global.css';
+
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <html lang="en-US">
       <Outlet />
-    </>
+    </html>
   ),
   notFoundComponent: () => <h1>404: Not Found</h1>,
 });
