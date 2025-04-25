203cda08ec8372181a8a143ff634b09538edb653
KristopherWagner <kmwagner91@gmail.com>
2025-04-25 15:23:02 -0400

    :sparkles: Use tanstack to serve index page

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      plugins: [
+        TanStackRouterRspack({
+          routeFileIgnorePattern: '.module.css',
+          target: 'react',
+          autoCodeSplitting: true,
+        }),
+      ],
+    },
+  },
 });
