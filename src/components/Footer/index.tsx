10cebaad071aea4018943a4b497fb26a0b0919cf
KristopherWagner <kmwagner91@gmail.com>
2025-05-21 17:23:31 -0400

    :wheelchair: Use a list instead of a div

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer({ children }: React.PropsWithChildren<{}>) {
   return (
     <footer className={styles.footer}>
-      <div>{children}</div>
+      <ul>{children}</ul>
     </footer>
   );
 }
