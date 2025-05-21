88166ea00392104eeca158837f5111e8b5f7e87c
KristopherWagner <kmwagner91@gmail.com>
2025-05-21 16:15:35 -0400

    :sparkles: Add navigation

---
+++ src/components/Footer/HomeLink.tsx
@@ -0,0 +1,11 @@
+import { Link } from '@tanstack/react-router';
+
+import { Route } from '../../routes/index';
+
+export default function HomeLink() {
+  return (
+    <Link aria-label="Return home" preload="intent" to={Route.to}>
+      <i className="fa-duotone fa-house fa-2x fa-fw" />
+    </Link>
+  );
+}
