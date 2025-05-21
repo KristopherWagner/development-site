edde63adb48f222ef4f64bb0fc44789a9b5fff1d
KristopherWagner <kmwagner91@gmail.com>
2025-05-21 15:14:54 -0400

    :recycle: Use a shared style and Link

--- src/routes/cookbook/recipes/walnut-pasta.tsx
+++ src/routes/cookbook/recipes/walnut-pasta.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
-import Seo from '../../../components/SEO';
 
 import recipe from '../../../../static/recipes/walnutPasta.json';
+
+import Seo from '../../../components/SEO';
 
 function WalnutPasta() {
   return (
@@ -12,34 +13,36 @@ function WalnutPasta() {
         title="Walnut Pasta"
         url="https://kwagner.dev/cookbook/recipes/walnut-pasta"
       />
-      <h1>{recipe.title}</h1>
-      <p>Makes {recipe.servings} servings</p>
-      <p>Total time: {recipe.time}</p>
-      <p>Tags:</p>
-      <ul>
-        {recipe.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-      <h2>Ingredients</h2>
-      <ul>
-        {recipe.ingredients.map((ingredient) => (
-          <li key={ingredient.ingredient}>
-            {ingredient.quantity} {ingredient.unit} {ingredient.ingredient}
-          </li>
-        ))}
-      </ul>
-      <h2>Instructions</h2>
-      <ol>
-        {recipe.instructions.map((step) => (
-          <li key={step}>{step}</li>
-        ))}
-      </ol>
-      <h2>Source</h2>
-      <p>
-        <a href={recipe.source.href}>{recipe.source.title}</a>
-      </p>
-      <p>{recipe.source.citation}</p>
+      <main className="main">
+        <h1>{recipe.title}</h1>
+        <p>Makes {recipe.servings} servings</p>
+        <p>Total time: {recipe.time}</p>
+        <p>Tags:</p>
+        <ul>
+          {recipe.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+        <h2>Ingredients</h2>
+        <ul>
+          {recipe.ingredients.map((ingredient) => (
+            <li key={ingredient.ingredient}>
+              {ingredient.quantity} {ingredient.unit} {ingredient.ingredient}
+            </li>
+          ))}
+        </ul>
+        <h2>Instructions</h2>
+        <ol>
+          {recipe.instructions.map((step) => (
+            <li key={step}>{step}</li>
+          ))}
+        </ol>
+        <h2>Source</h2>
+        <p>
+          <a href={recipe.source.href}>{recipe.source.title}</a>
+        </p>
+        <p>{recipe.source.citation}</p>
+      </main>
     </>
   );
 }
