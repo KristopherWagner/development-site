e9b2652de3976099532893dd42d81141b010cf56
KristopherWagner <kmwagner91@gmail.com>
2025-05-21 16:54:37 -0400

    :lipstick: Resize footer

--- src/components/Footer/CookbookLink.tsx
+++ src/components/Footer/CookbookLink.tsx
@@ -5,7 +5,7 @@ import { Route } from '../../routes/cookbook';
 export default function CookbookLink() {
   return (
     <Link aria-label="Return to cookbook" preload="intent" to={Route.to}>
-      <i className="fa-duotone fa-book-open-cover fa-2x fa-fw" />
+      <i className="fa-duotone fa-book-open-cover fa-fw" />
     </Link>
   );
 }
