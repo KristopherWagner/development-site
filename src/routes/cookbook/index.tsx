cdd0de17a8eb39e4a8c7c901536bab812da19a84
KristopherWagner <kmwagner91@gmail.com>
2025-05-28 17:07:54 -0400

    :bug: Apply style only to cookbook

--- src/routes/cookbook/index.tsx
+++ src/routes/cookbook/index.tsx
@@ -1,10 +1,12 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
+import clsx from 'clsx';
 
 import Footer from '../../components/Footer';
 import HomeLink from '../../components/Footer/HomeLink';
 import Seo from '../../components/SEO';
 
 import recipes from './recipes.json';
+import styles from './cookbook.module.css';
 
 export default function Cookbook() {
   return (
@@ -15,7 +17,7 @@ export default function Cookbook() {
         title="Wagner Family Cookbook"
         url="https://kwagner.dev/cookbook"
       />
-      <main className="main">
+      <main className={clsx('main', styles.cookbook)}>
         <h1>Cookbook</h1>
         <p>
           This page will slowly aggregate our favorite recipes. I'll (hopefully)
