d43272a05f3aa7a961e91ef9e77ad5853c008833
Kristopher Wagner <kmwagner91@gmail.com>
2025-10-17 18:24:28 -0400

    :sparkles: Add Not Found page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,9 @@ import '@fontsource/montserrat';
 import '@fontsource/roboto';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 
+import NotFound from '../components/NotFound';
+
 export const Route = createRootRoute({
   component: () => <Outlet />,
-  notFoundComponent: () => <h1>404: Not Found</h1>,
+  notFoundComponent: NotFound,
 });
