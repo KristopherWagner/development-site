2851510377722693395c71aad29b7a868b28972f
Kristopher Wagner <kmwagner91@gmail.com>
2025-10-29 11:51:25 -0400

    :bug: Import JSON correctly

--- src/routes/cookbook/$recipeName.tsx
+++ src/routes/cookbook/$recipeName.tsx
@@ -8,7 +8,7 @@ import Header from '../../components/Header';
 import HomeLink from '../../components/Footer/HomeLink';
 
 import { Route as cookbookRoute } from '.';
-import recipes from './recipes.json';
+import recipes from './recipes.json' with { type: 'json' };
 
 import styles from './cookbook.module.css';
 
