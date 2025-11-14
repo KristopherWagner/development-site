7db8b4511239bffb0467ac8f857c07efe6b54124
Kristopher Wagner <kmwagner91@gmail.com>
2025-11-14 13:36:35 -0500

    :bug: fix build

--- wedding/src/bootstrap.tsx
+++ wedding/src/bootstrap.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import WeddingPage from "./wedding";
 
-import "./index.css";
-
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
