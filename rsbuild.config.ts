e2b3260a25365cb16efd8a06d28463b6a349d3f0
Kristopher Wagner <kmwagner91@gmail.com>
2025-11-24 14:39:07 -0500

    :sparkles: Add types for css modules

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
 import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
   html: {
     template: './static/index.html',
   },
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginTypedCSSModules()],
   tools: {
     rspack: {
       plugins: [
