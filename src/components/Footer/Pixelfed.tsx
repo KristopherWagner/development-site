f705a98a95749ed37e7a9d8c3f8874c24b8e9b9a
Kristopher Wagner <kmwagner91@gmail.com>
2025-12-02 16:55:18 -0500

    :sparkles: Add bluesky link #55

---
+++ src/components/Footer/Pixelfed.tsx
@@ -0,0 +1,13 @@
+export default function Pixelfed() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      height="1em"
+      width="1.25em"
+      viewBox="0 0 512 512"
+    >
+      <path d="M256 0a256 256 0 1 1 0 512 256 256 0 1 1 0-512zM235.7 311.9l47 0c44.2 0 80.1-34.9 80.1-78s-35.9-78-80.1-78l-67.8 0c-25.5 0-46.2 20.1-46.2 45l0 175.1 67-64.1z" />
+    </svg>
+  );
+}
