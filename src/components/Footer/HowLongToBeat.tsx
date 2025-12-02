ca54fc217527f1f23c8d28fe11a5cf7b9908232e
Kristopher Wagner <kmwagner91@gmail.com>
2025-12-02 17:03:59 -0500

    :sparkles: Add HowLongToBeat #55

---
+++ src/components/Footer/HowLongToBeat.tsx
@@ -0,0 +1,13 @@
+export default function HowLongToBeat() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      height="1em"
+      width="1.25em"
+      viewBox="0 0 384 512"
+    >
+      <path d="M320 288l0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-384c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-256 0 0-160c0-17.7-14.3-32-32-32S0 46.3 0 64L0 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 256 0z" />
+    </svg>
+  );
+}
