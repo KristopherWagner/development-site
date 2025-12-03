06b58354340a629a2811bff6044f4c1a6e51fc01
Kristopher Wagner <kmwagner91@gmail.com>
2025-12-03 11:27:40 -0500

    :fire: Remove whitespace causing a :bug: display bug

--- src/components/Footer/CookbookLink.tsx
+++ src/components/Footer/CookbookLink.tsx
@@ -6,7 +6,7 @@ export default function CookbookLink() {
   return (
     <li>
       <Link aria-label="Return to cookbook" preload="intent" to={Route.to}>
-        <i className="fa-duotone fa-book-open-cover fa-fw" />{' '}
+        <i className="fa-duotone fa-book-open-cover fa-fw" />
         <span>Cookbook</span>
       </Link>
     </li>
