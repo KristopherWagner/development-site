c6b267253d95b0678876bf21d6aade1fd4a2069a
KristopherWagner <kmwagner91@gmail.com>
2026-04-14 08:09:07 -0400

    :bug: Fix issues with grid

--- src/components/QuoteCard/index.tsx
+++ src/components/QuoteCard/index.tsx
@@ -1,10 +1,17 @@
 import clsx from 'clsx';
 import styles from './QuoteCard.module.css';
 
-export default function QuoteCard({ children }: React.PropsWithChildren<{}>) {
+export default function QuoteCard({
+  as = 'blockquote',
+  className,
+  children,
+}: React.PropsWithChildren<{ as?: 'blockquote' | 'div'; className?: string }>) {
+  const Component = as;
   return (
-    <blockquote className={clsx(styles.block, styles['block-testimonial'])}>
+    <Component
+      className={clsx(styles.block, styles['block-testimonial'], className)}
+    >
       {children}
-    </blockquote>
+    </Component>
   );
 }
